d25e9266b8640700d7258c8ab392cc71e674e3fa
Karthika Punnengattu Padi Subramanian <76861188+Karthika-ps@users.noreply.github.com>
2026-02-03 16:35:46 +0530

    Create api app.py

---
+++ api_gateway/app.py
@@ -0,0 +1,61 @@
+from flask import Flask, request, jsonify
+import requests
+
+app = Flask(__name__)
+
+ML_SERVICE_URL = "http://127.0.0.1:8000/predict"
+LLM_SERVICE_URL = "http://127.0.0.1:7000/explain"
+
+
+@app.route("/health", methods=["GET"])
+def health():
+    return jsonify({"status": "API gateway running"}), 200
+
+
+@app.route("/analyze", methods=["POST"])
+def analyze_ticket():
+    data = request.get_json()
+
+    if not data or "text" not in data:
+        return jsonify({"error": "Missing 'text' field"}), 400
+
+    ticket_text = data["text"]
+
+    # --------------------------------------------------
+    # Call ML service
+    # --------------------------------------------------
+    ml_response = requests.post(
+        ML_SERVICE_URL,
+        json={"text": ticket_text},
+        timeout=5
+    )
+
+    ml_result = ml_response.json()
+
+    # --------------------------------------------------
+    # Call LLM service
+    # --------------------------------------------------
+    llm_response = requests.post(
+        LLM_SERVICE_URL,
+        json={
+            "text": ticket_text,
+            "priority": ml_result["priority"],
+            "confidence": ml_result["confidence"]
+        },
+        timeout=5
+    )
+
+    explanation = llm_response.json()["explanation"]
+
+    # --------------------------------------------------
+    # Final response
+    # --------------------------------------------------
+    return jsonify({
+        "priority": ml_result["priority"],
+        "confidence": ml_result["confidence"],
+        "explanation": explanation
+    }), 200
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
